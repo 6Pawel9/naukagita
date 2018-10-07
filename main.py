bdddd8905f51f60657624dd3de1fd1761658f52e
6Pawel9 <6Pawel9@users.noreply.github.com>
2018-10-07 14:23:01 +0200

    Update pyton

--- main.py
+++ main.py
@@ -14,8 +14,8 @@
 #else:
 #    print "Ojej"
 
-samochody = ['syrena', 'polonez']
-ilosc = [3, 5]
+#samochody = ['syrena', 'polonez']
+#ilosc = [3, 5]
 
 #for s in samochody:
 #    print(s)
@@ -30,8 +30,38 @@ ilosc = [3, 5]
 
 #print(len(ilosc))
 
-print("Dlugosc: {0}".format(len(samochody)))
+#print("Dlugosc: {0}".format(len(samochody)))
 
-for idx in range(len(samochody)):
-    print ("idx" + str(idx) + ": " + samochody[idx])
-    print (samochody[idx] + " na ilosc dzwi " + str(ilosc[idx]))
+#for idx in range(len(samochody)):
+#    print ("idx" + str(idx) + ": " + samochody[idx])
+#    print (samochody[idx] + " na ilosc dzwi " + str(ilosc[idx]))
+
+
+#samolot = {'name' : 'boing', 'przebieg' : 1000 , 'type' : 'pasarzerski'}
+
+#print(samolot['name'])
+#for key, value in samolot.iteritems():
+#    print("{0}:{1}".format(key,value))
+
+#for key in samolot:
+#    print("{0}:{1}, key, samolot[key]")
+
+
+#pokemony = {'type' : 'ognisty', 'atak': 500, 'name' : 'Charmander' }
+
+#print(pokemony['name'])
+#for key, value in pokemony.iteritems():
+#    print("{0}:{1}".format(key,value))
+
+#for key in pokemony:
+#    print("{0}:{1}, key, pokemony[key]")
+
+def print_dict(d):
+    for key, value in d.iteritems():
+        print("{0}:{1}".format(key,value))
+
+if __name__ == "__main__":
+      samolot = {'name' : 'boing', 'przebieg' : 1000 , 'type' : 'pasarzerski'}
+
+      print_dict(samolot)
+      print_dict(samolot)
